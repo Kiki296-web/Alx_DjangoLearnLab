cd13e6edbbfd9526af47393506c27b6ee80630ce
jonasnikitha <jonasnikitha@gmail.com>
2025-07-20 12:54:25 +0200

    Updated the views and urls files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+
+from .models import Book
+from .models import Library
 
 # Create your views here.
 def book_list_view(request):
@@ -11,24 +12,18 @@ def book_list_view(request):
 # Class-Based View: show a specific Library + its books
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "relationship_app/library_detail.html"  
-    context_object_name = "library" 
-    
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         library = self.object
+        
+        try:
+            books = library.books.all() 
+        except AttributeError:
+            books = library.book_set.all()
 
-
-        if hasattr(library, "books"):
-            books_qs = library.books.all()
-        elif hasattr(library, "book_set"):  
-            books_qs = library.book_set.all()
-        else:
-            books_qs = Book.objects.none()
-
-    
-        books_qs = books_qs.select_related("author")
-
-        context["books"] = books_qs
+        context["books"] = books
         return context
   
