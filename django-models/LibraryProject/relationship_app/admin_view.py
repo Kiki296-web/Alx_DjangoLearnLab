47bc080547f498c5683ba1d964f66f03fd965d44
jonasnikitha <jonasnikitha@gmail.com>
2025-07-20 15:01:46 +0200

    Added more views and templates on the project

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import user_passes_test
 from .utils import is_admin
+from django.shortcuts import render
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return HttpResponse("Welcome to the Admin Dashboard!")
+    return render(request, 'admin_view.html')
--- django-models/LibraryProject/relationship_app/librarian_view.py
+++ django-models/LibraryProject/relationship_app/librarian_view.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import user_passes_test
 from .utils import is_librarian
+from django.shortcuts import render
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return HttpResponse("Welcome to the Librarian Dashboard!")
+    return render(request, 'librarian_view.html')
