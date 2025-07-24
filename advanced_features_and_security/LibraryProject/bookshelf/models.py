c1166b3329ab640c9a87a218983ef7534fd90c9b
jonasnikitha <jonasnikitha@gmail.com>
2025-07-24 21:44:41 +0200

    Implemented a Custom User Model in my Django app

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, UserManager, PermissionsMixin, AbstractBaseUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin, AbstractBaseUser
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.contrib.auth import get_user_model
@@ -19,7 +19,7 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
-class CustomUserManager(UserManager):
+class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password, date_of_birth=None, profile_photo=None, **extra_fields):
         if not email:
             raise ValueError("You have not entered a valid email")
