e518e45dcd4c38b32299f7bd727303793394108d
jonasnikitha <jonasnikitha@gmail.com>
2025-07-24 21:25:18 +0200

    Implemented a Custom User Model in my Django app

---
+++ advanced_features_and_security/LibraryProject/relationship_app/bookshelf/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('publication_year', 'author')
+    search_fields = ('title', 'author')
+    
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+    search_fields = ['username', 'email']
+    list_filter = ['is_staff', 'is_superuser']
+
+
+
+
+
+
+# Register your models here.
+admin.site.register(Book)
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ advanced_features_and_security/LibraryProject/relationship_app/bookshelf/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager, PermissionsMixin, AbstractBaseUser
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length = 100)
+    published_year = models.IntegerField()
+    
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    
+    def __str__(self):
+        return self.username
+
+class CustomUserManager(UserManager):
+    def create_user(self, username, email, password, date_of_birth=None, profile_photo=None, **extra_fields):
+        if not email:
+            raise ValueError("You have not entered a valid email")
+        
+        email =self.normalize_email(email)
+        user = user.model(username=username,
+                          email=email,
+                          date_of_birth=date_of_birth,
+                          profile_photo=profile_photo,
+                          **extra_fields)
+    
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+    
+        return self.create_user(email, password, **extra_fields)
+        
+    def create_superuser(self, username, email, password, **extra_fields):
+       extra_fields.setdefault('is_staff', True)
+       extra_fields.setdefault('is_superuser', True)
+       return super().create_superuser(username, email, password, **extra_fields)
+   
+class User(AbstractBaseUser, PermissionsMixin):
+    email =models.EmailField(blank=True, default='', unique=True)
+    name =models.CharField(max_length=255, blank=True, default='')
+    
+    is_active =models.BooleanField(default=True)
+    is_superuser =models.BooleanField(default=False)
+    is_staff =models.BooleanField(default=False)
+    
+    date_joined =models.DateTimeField(default=timezone.now)
+    last_login =models.DateTimeFieldt(blank=True, null=True)
+    
+    objects =CustomUserManager()
+    
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELD =[]
+    
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'User'
