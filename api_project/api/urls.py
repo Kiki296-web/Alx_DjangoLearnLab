678ebeb02a78684406a08ec2e07b49004916cea0
jonasnikitha <jonasnikitha@gmail.com>
2025-07-30 20:22:47 +0200

    Built my First API Endpoint with Django REST Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from .views import BookList
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
-    path('api/', include('api.urls')),
 ]
