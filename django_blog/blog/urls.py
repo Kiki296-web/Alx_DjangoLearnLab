fe6223f86a8bae82f43c6b4fd97e0c33bc8c5f21
jonasnikitha <jonasnikitha@gmail.com>
2025-08-15 19:45:52 +0200

    Fixed error in blog/urls.py file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,7 +17,8 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('post/<int:post_id>/comment/add/', views.add_comment, name='add_comment'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'), 
     path('comment/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='edit_comment'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete_comment'),
     
